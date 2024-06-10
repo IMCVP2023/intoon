65f9c527070b37a511f5c66e40f51ca88e8caddb
new-crystal <oj3339@naver.com>
2024-06-10 13:44:59 +0900

    [240610] add favicon icon

--- main/board_notice_detail.php
+++ main/board_notice_detail.php
@@ -133,5 +133,11 @@
 		-->
 	</div>
 </section>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jQuery-rwdImageMaps/1.6/jquery.rwdImageMaps.min.js"></script>
 
+<script>
+$(document).ready(function () {
+          $('img[usemap]').rwdImageMaps()  
+})
+</script>
 <?php include_once('./include/footer.php');?>
